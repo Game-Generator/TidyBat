f426b14305e44aaa541458b25a31d1d2fab32d39
Game-Generator <79217012+Game-Generator@users.noreply.github.com>
2021-02-17 18:23:53 +0100

    Added Batch file for clear DNS Cache

---
+++ BatchFiles/ClearDNSCache.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Clears DNS Cache, eventually needs admin rights
+rem In the cmd with "ipconfig /displaydns" you can look for your currently saved DNS
+rem It can held when old IP settings make a Conection difficult and makes the PC more secure from DNS attacks
+ipconfig /flushdns
