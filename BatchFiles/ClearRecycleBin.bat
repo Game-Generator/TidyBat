cb1043aa8ef93d8e0e6c9214f22a77ed3d999975
Game-Generator <79217012+Game-Generator@users.noreply.github.com>
2021-02-17 19:08:35 +0100

    Update ClearRecycleBin.bat

--- BatchFiles/ClearRecycleBin.bat
+++ BatchFiles/ClearRecycleBin.bat
@@ -1,8 +1,8 @@
 @echo off
 REM Clear Recycle Bin
 
-REM -> Needs admin rights
-REM -> There could be more then one RecycleBin, then you need to modify
+REM Needs admin rights
+REM There could be more then one RecycleBin, then you need to modify
 rd /s /q c:\$Recycle.Bin
 REM Executing twice (maybe necessary)
 rd /s /q c:\$Recycle.Bin
