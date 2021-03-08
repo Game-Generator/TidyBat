2cfdd0b5d7c870247c2b76036de2ef01654f465a
Game-Generator <79217012+Game-Generator@users.noreply.github.com>
2021-03-08 20:03:20 +0100

    Also delete Folders in Temp

--- BatchFiles/ClearStandardTemp.bat
+++ BatchFiles/ClearStandardTemp.bat
@@ -1,6 +1,7 @@
 @echo off
 Rem Try to delte all Files and folders in the defined Temp folder (environment variable)
 del /q/f/s %TEMP%\*
+for /D %%i in ("%TEMP%\*") do RD /S /Q "%%i"
 Rem Can only delete stuff taht is not in use
 Rem outcomment to see what was delted and what not:
 Rem pause
