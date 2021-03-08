4118032d05c8269acb17d7d0e6ecf2ac4ad9eb38
Game-Generator <79217012+Game-Generator@users.noreply.github.com>
2021-03-08 20:29:37 +0100

    Possible Feedback which folders will be deleted (outcommented)

--- BatchFiles/ClearStandardTemp.bat
+++ BatchFiles/ClearStandardTemp.bat
@@ -1,6 +1,8 @@
 @echo off
 Rem Try to delete all files and folders in the defined Temp folder (environment variable)
 del /q/f/s %TEMP%\*
+Rem Log folders:
+Rem for /D %%i in ("%TEMP%\*") do echo Delete folder and its content: %%i
 for /D %%i in ("%TEMP%\*") do RD /S /Q "%%i"
 Rem Can only delete stuff that is not in use
 Rem Outcomment to see what was delted and what not:
